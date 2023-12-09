934a7303ebd7fb93d6e35af51560d0185f1ff824
firdaus <a.firdaus@gmail.com>
2023-12-09 21:34:51 +0700

    add routes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const courseRouter = require('./routes/course')
+const mediaRouter = require('./routes/media')
+const orderRouter = require('./routes/order')
+const paymentRouter = require('./routes/payment')
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,5 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/course', courseRouter)
+app.use('/media', mediaRouter)
+app.use('/order', orderRouter)
+app.use('/payment', paymentRouter)
 
 module.exports = app;
